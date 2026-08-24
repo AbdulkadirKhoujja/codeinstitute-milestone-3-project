424a71db69645917c553886c1e0aa83c9bdbaecc
AbdulkadirKhoujja <abdulkhoujja25@outlook.com>
2026-08-24 14:46:14 +0100

    feat: register posts in admin

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Post
 
 
 @admin.register(Category)
@@ -9,3 +9,20 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("name", "description")
     prepopulated_fields = {"slug": ("name",)}
     ordering = ("name",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "author",
+        "category",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("title", "summary", "content", "author__username")
+    list_filter = ("status", "category", "created_at")
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)
+    list_select_related = ("author", "category")
