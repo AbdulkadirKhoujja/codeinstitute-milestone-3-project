d6d7a9a0a6bb8d2417131caa7c839371e0365dbb
AbdulkadirKhoujja <abdulkhoujja25@outlook.com>
2026-08-24 14:35:30 +0100

    feat: add category model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """A topic used to organise related news posts."""
+
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
